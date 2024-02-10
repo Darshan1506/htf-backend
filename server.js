e88097f6ea3074463e433ec4e3a7754cdddb0550
Darshan Rajpurohit <darshanrajpurohit47@gmail.com>
2024-02-11 05:10:18 +0530

    please

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
+// Remove this line if you are using your custom CORS middleware
 app.use(cors());
 app.use(express.json());
 // question to audio , it will be question sent to the backend it will
